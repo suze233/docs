83082bc73904b7ed5df5687bfe7843e392f13465
suze <kunliang.me@outlook.com>
2023-12-27 18:47:26 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LLMGNAS'
+# copyright = '2023'
+author = 'Suze'
 
 release = '0.1'
 version = '0.1.0'
